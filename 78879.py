b0ce45fb98ecad424f08f00676e7b1a26ac9fdc3
mahan <sepanloooo@gmail.com>
2024-07-17 20:06:51 +0330

    end

--- 78879.py
+++ 78879.py
@@ -1,14 +1,22 @@
-
+from datetime import datetime
 def Level():
-    level = 1
     level = int(input("choose alevel between 1,2 and 3: "))       
     while True:
-        if level < 1 or level > 3:
+        if level < 0 or level > 3:
             level = int(input("wrong\n choose alevel between 1,2 and 3: "))        
         else:
             break
         
-
+    # if level == 0:
+    #     board = [[5,3,4,6,7,8,9,1,2],            
+    #              [6,7,2,1,9,5,3,4,8],  
+    #              [1,9,8,3,4,2,5,6,7],  
+    #              [8,5,9,7,6,1,4,2,3],  
+    #              [4,2,6,8,5,3,7,9,1],  
+    #              [7,1,3,9,2,4,8,5,6],  
+    #              [9,6,1,5,3,7,2,8,4],  
+    #              [2,8,7,4,1,9,6,3,5],
+    #              [3,4,5,2,8,6,1,7,0]]
    
     if level == 1:
         board = [[8, 0, 0, 9, 1, 3, 4, 0, 0],            
@@ -47,6 +55,7 @@ def Level():
     return board
 
 def is_valid_move(board, row, col, num):  #اعتبار سنجی جدول 
+    
     if num in board[row]:  #چک کردن سطر ها
         # print(f"row!!!{board[row].index(num)+1}")
         return False  
@@ -73,33 +82,38 @@ def is_winner(board):  #چک کردن پیروزی
 
 def iterbord(board):  
     for row in board:  
-        print (row)
+        yield (row)
 
-def find_conflict(board, num, row, col):  
+def check_conflict(board, row, col, num):  
+    conflicts = {
+        "row":[],
+        "col":[],
+        "Square":[]
+    }
+
     for i in range(9):  
-        if i == row - 1:
-            if num in board[i]:  # چک کردن در سطر ها  
-                roww = i + 1  # شماره سطر  
-                coll = board[i].index(num) + 1  # شماره ستون  
-                print(f"!!!!!!!!!!Invalid move.!!!!!!!!!!!!!!! The number {num}  in row *{roww}* or column *{coll}*. Please try again.")  
-                break
+        if board[row][i] == num:  
+            conflicts["row"].append((row+1, i+1))  
+
+    for i in range(9):  
+        if board[i][col] == num:  
+            conflicts["col"].append((i+1, col+1))  
+
     
-    for j in range(9): 
-        if j == col - 1: 
-            if num in [board[i][j] for i in range(9)]:  # چک کردن در ستون ها  
-                roww = [board[i][j] for i in range(9)].index(num) + 1  # شماره سطر  
-                coll = j + 1  # شماره ستون  
-                print(f"!!!!!!!!!!Invalid move.!!!!!!!!!!!!!!! The number {num}  in row *{roww}* or column *{coll}*. Please try again.")  
-                break
-    for i in range(0, 9, 3):  
-        for j in range(0, 9, 3):  
-            if num in [board[r][c] for r in range(i, i+3) for c in range(j, j+3)]:  
-                row = [board[r][c] for r in range(i, i+3) for c in range(j, j+3)].index(num) // 3 + i + 1  # شماره سطر  
-                col = [board[r][c] for r in range(i, i+3) for c in range(j, j+3)].index(num) % 3 + j + 1  # شماره ستون  
-            print(f"!!!!!!!!!!Invalid move.!!!!!!!!!!!!!!! The number {num}  in row *{row}* or column *{col}*. Please try again.")  
-            break
-    return None
+    square_row = ((row) // 3) * 3  
+    square_col = ((col) // 3) * 3  
+
+    for i in range(3):  
+        for j in range(3):  
+            if board[square_row + i][square_col + j] == num:  
+                conflicts["Square"].append((square_row + i+1, square_col + j+1))  
+
+    return conflicts  
+
+    
+
 def play_sudoku():   # بازی 
+    t1 = datetime.now()
     board=Level()
     print("\nEmpty Sudoku Board:")  
     print(f"""
@@ -119,16 +133,53 @@ def play_sudoku():   # بازی
     iterbord(board)  
     
     while not is_winner(board):  
-        row = int(input("Enter row number (1-9): ")) - 1  
-        col = int(input("Enter column number (1-9): ")) - 1  
-        num = int(input("Enter number to fill (1-9): ")) 
-        conflict = find_conflict(board, num, row, col)  
-        if conflict:  
-            print(f"Conflict with row {conflict} and column {conflict}. Please select a different number.")
+        row = (input("Enter row number (1-9): ")) 
+        col = (input("Enter column number (1-9): "))  
+        num = (input("Enter number to fill (1-9): ")) 
+        if row.isdigit() and col.isdigit() and num.isdigit():
+            row = int(row)-1
+            col = int(col)-1
+            num = int(num)
+            if 0 <= row < 9 and 0 <= col < 9 and 1 <= num <= 9:
+                 pass
+            else:
+                print("Please enter numbers within the range (1-9).")
+                continue
+        else:
+            print("Please enter numeric values.")
+            continue
+
+        if board[row][col] != 0:
+            print(f'you cant change row {row + 1} and col {col + 1}')
+            continue
+
+
         if is_valid_move(board, row, col, num):  
             board[row][col] = num  
+            print("Updated Sudoku Board:") 
 
-        print("Updated Sudoku Board:")  
+        else:
+            conflicts = check_conflict(board, row, col, num)  
+            if conflicts:  
+                print(f"Conflict found for number {num} at row {row+1} and column {col+1} whit : {conflicts}")
+
+        print(f"""
+        {board[0][0]} {board[0][1]} {board[0][2]} | {board[0][3]}  {board[0][4]}  {board[0][5]} | {board[0][6]}  {board[0][7]}  {board[0][8]} 
+        {board[1][0]} {board[1][1]} {board[1][2]} | {board[1][3]}  {board[1][4]}  {board[1][5]} | {board[1][6]}  {board[1][7]}  {board[1][8]} 
+        {board[2][0]} {board[2][1]} {board[2][2]} | {board[2][3]}  {board[2][4]}  {board[2][5]} | {board[2][6]}  {board[2][7]}  {board[2][8]} 
+        -------------------------
+        {board[3][0]} {board[3][1]} {board[3][2]} | {board[3][3]}  {board[3][4]}  {board[3][5]} | {board[3][6]}  {board[3][7]}  {board[3][8]} 
+        {board[4][0]} {board[4][1]} {board[4][2]} | {board[4][3]}  {board[4][4]}  {board[4][5]} | {board[4][6]}  {board[4][7]}  {board[4][8]} 
+        {board[5][0]} {board[5][1]} {board[5][2]} | {board[5][3]}  {board[5][4]}  {board[5][5]} | {board[5][6]}  {board[5][7]}  {board[5][8]} 
+        -------------------------
+        {board[6][0]} {board[6][1]} {board[6][2]} | {board[6][3]}  {board[6][4]}  {board[6][5]} | {board[6][6]}  {board[6][7]}  {board[6][8]} 
+        {board[7][0]} {board[7][1]} {board[7][2]} | {board[7][3]}  {board[7][4]}  {board[7][5]} | {board[7][6]}  {board[7][7]}  {board[7][8]} 
+        {board[8][0]} {board[8][1]} {board[8][2]} | {board[8][3]}  {board[8][4]}  {board[8][5]} | {board[8][6]}  {board[8][7]}  {board[8][8]} 
+
+""") 
         iterbord(board)  
     print("amazing! You have won the game!")  
+    t2 = datetime.now()
+    T =t2 - t1
+    print(f'Game duration:{T}')
 play_sudoku()
